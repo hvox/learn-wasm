f8fc796e8778681a070cd1547e5725fda11eea11
Uladzislau Khamkou <kham@tuta.io>
2023-12-30 22:55:05 +0300

    Add automatic wat to wasm conversion

--- run_wasm.ts
+++ run_wasm.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env -S deno run -A
 
-const wasmCode = await Deno.readFile(Deno.args[0]);
+const wasmSignature = new Uint8Array([0, 97, 115, 109]);
+let wasmCode = await Deno.readFile(Deno.args[0]);
+if (!arraysAreEqual(wasmCode.slice(0, 4), wasmSignature)) {
+	const wasmPath = await Deno.makeTempFile();
+	const cmp = ["wat2wasm", Deno.args[0], "-o", wasmPath];
+	await Deno.run({ cmd: cmp }).status();
+	wasmCode = await Deno.readFile(wasmPath);
+}
 const wasmModule = new WebAssembly.Module(wasmCode);
 const imports = {
 	log: (x: number) => console.log(x),
@@ -12,3 +19,8 @@ const imports = {
 const wasmInstance = new WebAssembly.Instance(wasmModule, { imports });
 const main = wasmInstance.exports.main as CallableFunction;
 console.log("RESULT:", main());
+
+function arraysAreEqual(arr1: Uint8Array, arr2: Uint8Array): boolean {
+	if (arr1.length != arr2.length) return false;
+	return arr1.every((value, index) => value === arr2[index]);
+}
